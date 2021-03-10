de073549e1ff6724c2378580fb716da7a52a30a8
dante <sack_dante@hotmail.com>
2021-03-10 08:54:11 -0300

    setup redux store

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { combineReducers, createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import productsReducer from './redux/reducers/productsReducer'
+
+const rootReducer = combineReducers({
+    products: productsReducer,
+})
+
+const store = createStore(rootReducer)
+
 export default function App() {
     return (
-        <View style={styles.screen}>
-            <Text>hello world</Text>
-        </View>
+        <Provider store={store}>
+            <View style={styles.screen}>
+                <Text>hello world</Text>
+            </View>
+        </Provider>
     )
 }
 
---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { FlatList } from 'react-native'
+
+export default function ProductsOverviewScreen(props) {
+    return (
+        <FlatList>
+
+        </FlatList>
+    )
+}
