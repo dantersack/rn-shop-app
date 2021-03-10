3815bb01512703d56e3d42c9dce33578f3f6e8c2
dante <sack_dante@hotmail.com>
2021-03-10 13:11:27 -0300

    setup stack navigator

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import productsReducer from './redux/reducers/productsReducer'
+import ShopNavigator from './navigation/ShopNavigator'
 
 const rootReducer = combineReducers({
     products: productsReducer,
@@ -15,9 +16,7 @@ const store = createStore(rootReducer)
 export default function App() {
     return (
         <Provider store={store}>
-            <View style={styles.screen}>
-                <Text>hello world</Text>
-            </View>
+            <ShopNavigator />
         </Provider>
     )
 }
