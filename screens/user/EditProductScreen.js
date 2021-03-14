84d846332fd0b370f9c128dc98fc35b14937fd20
dante <sack_dante@hotmail.com>
2021-03-14 12:43:25 -0300

    add edit/add product form

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,14 +1,104 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { Platform, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
 
-export default function EditProductScreen() {
+import { useSelector } from 'react-redux'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+
+import CustomHeaderButton from '../../components/UI/HeaderButton'
+
+export default function EditProductScreen(props) {
+    const productId = props.navigation.getParam('productId')
+    const editedProduct = useSelector(state => state.products.userProducts.find(prod => prod.id === productId))
+    
+    const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
+    const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '')
+    const [price, setPrice] = useState(editedProduct ? editedProduct.price.toFixed(2) : '')
+    const [description, setDescription] = useState(editedProduct ? editedProduct.description : '')
+
+    const submitHandler = useCallback(() => {
+        console.log('submitting...')
+    }, [])
+
+    useEffect(() => {
+        props.navigation.setParams({submit: submitHandler})
+    }, [submitHandler])
+
     return (
-        <View>
-            <Text>oh hi mark</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Title</Text>
+                    <TextInput 
+                        style={styles.input}
+                        value={title}
+                        onChangeText={text => setTitle(text)}
+                    />
+                </View>
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Image URL</Text>
+                    <TextInput 
+                        style={styles.input}
+                        value={imageUrl}
+                        onChangeText={text => setImageUrl(text)}
+                    />
+                </View>
+                {!editedProduct && (
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>Price</Text>
+                        <TextInput 
+                            style={styles.input} 
+                            value={price}
+                            onChangeText={text => setPrice(text)}
+                        />
+                    </View>
+                )}
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Description</Text>
+                    <TextInput
+                        style={[styles.input, {marginBottom: 5}]} 
+                        value={description}
+                        onChangeText={text => setDescription(text)}
+                    />
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
+EditProductScreen.navigationOptions = ({navigation}) => {
+    const submitFunction = navigation.getParam('submit')
+    return {
+        headerTitle: navigation.getParam('productId') ? 'Edit Product' : 'Add Product',
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item 
+                    title='Save'
+                    iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+                    onPress={submitFunction}
+                />
+            </HeaderButtons>
+        ),
+    }
+}
+
 const styles = StyleSheet.create({
-    
+    form: {
+        margin: 20,
+        padding: 10,
+        borderRadius: 8,
+        backgroundColor: 'white',
+    },
+    formControl: {
+        width: '100%',
+    },
+    label: {
+        fontFamily: 'open-sans-bold',
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 4,
+        marginBottom: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ccc',
+    },
 })
